3c86a10b4f1e037bb2b7e5a35322a00dd14c7a77
hann2613 <hann2626@163.com>
2024-07-25 15:34:22 +0100

    main

--- Group5_724/homepage.php
+++ Group5_724/homepage.php
@@ -10,18 +10,27 @@
 
 <body>
   <div id="app">
+    <?php
+    require('model/database.php');
+    require('model/course_db.php');
+    require('model/mentor_db.php');
+    require('model/live_db.php');
+
+    $courses = get_courses();
+    $mentors = get_mentors();
+    $lives = get_lives(3);
+    ?>
     <header>
       <nav>
         <div class="logo">StudentPreneur</div>
-        <div class="icons">
-          <button class="icons">Sign In</button>
-          <button class="icons">Sign Up</button>
-        </div>
+        <a href="learning.php">Learning</a>
+        <a href="event.php">Events</a>
+        <a href="globalConnections.php">Global Connections</a>
       </nav>
     </header>
     <div class="homeCover">
       <h1>Turn your ideas into reality</h1>
-      <button>Join us now</button>
+      <button @click="signIn">Join us now</button>
     </div>
 
     <!-- Mentors -->
@@ -30,12 +39,14 @@
       <div class="slide-container">
         <button class="prev" @click="prevSlide">&#10094;</button>
         <div class="mentor-slide">
-          <div class="mentor-card" v-for="mentor in mentors" :key="mentor.id">
-            <img :src="mentor.image" :alt="mentor.name" />
-            <h3>{{ mentor.name }}</h3>
-            <p>{{ mentor.description }}</p>
-            <button class="booksession">Book a Session</button>
-          </div>
+          <?php foreach ($mentors as $mentor) : ?>
+            <div class="mentor-card" key="<?php echo $mentor['id']; ?>">
+              <img src="<?php echo htmlspecialchars($mentor['avatar']); ?>" alt="mentor" />
+              <h3><?php echo htmlspecialchars($mentor['name']); ?></h3>
+              <p><?php echo htmlspecialchars($mentor['description']); ?></p>
+              <button @click="bookSession()">Book a Session</button>
+            </div>
+          <?php endforeach; ?>
         </div>
         <button class="next" @click="nextSlide">&#10095;</button>
       </div>
@@ -48,13 +59,15 @@
     <section class="live-session">
       <h2>Start Your Live Session</h2>
       <div class="live-list">
-        <div class="session-card" v-for="live in lives" :key="live.id">
-          <img :src="live.image" :alt="live.title" />
-          <h3>{{ live.title }}</h3>
-          <p>{{ live.date }}</p>
-          <button @click="remindMe(live.id)">Remind Me</button>
-          <button @click="joinSession(live.id)">Join this Session</button>
-        </div>
+        <?php foreach ($lives as $live) : ?>
+          <div class="session-card" key="<?php echo $live['id']; ?>">
+            <img src="<?php echo htmlspecialchars($live['image']); ?>" alt="live" />
+            <h3><?php echo htmlspecialchars($live['title']); ?></h3>
+            <p><?php echo htmlspecialchars($live['date']); ?></p>
+            <button @click="remindMe()">Remind Me</button>
+            <button @click="joinSession()">Join this Session</button>
+          </div>
+        <?php endforeach; ?>
       </div>
     </section>
 
@@ -63,7 +76,7 @@
     <section class="global-connections">
       <h2>Discover Global Connections</h2>
       <div class="map">
-        <img src="img/connection.png" />
+        <img src="" />
       </div>
     </section>
 
@@ -77,66 +90,27 @@
     new Vue({
       el: "#app",
       data: {
-        mentors: [{
-            id: 1,
-            name: "Jacob Jones",
-            image: "img/mentor1.png",
-            description: "Marketing Expert & Serial Entrepreneur",
-          },
-          {
-            id: 2,
-            name: "Emily Davis",
-            image: "img/mentor2.jpg",
-            description: "Funding Specialist & Startup Advisor",
-          },
-          {
-            id: 3,
-            name: "Michael Brown",
-            image: "img/mentor3.png",
-            description: "Innovation Consultant & Startup Founder",
-          },
-          {
-            id: 4,
-            name: "Timothée Chalamet",
-            image: "img/mentor4.png",
-            description: "Film & Art",
-          },
-        ],
-        lives: [{
-            id: 1,
-            title: "Engage Entrepreneurial Minds",
-            date: "July 20, 5:00 PM",
-            image: "img/live1.jpg",
-          },
-          {
-            id: 2,
-            title: "Pitch Perfect: How to Impress Investors",
-            date: "July 21, 7:00 PM",
-            image: "img/live2.jpg",
-          },
-          {
-            id: 3,
-            title: "Skills for Startup Success",
-            date: "July 19, 6:00 PM",
-            image: "img/live3.jpg",
-          },
-        ],
         currentSlide: 0,
+        mentors: <?php echo json_encode($mentors); ?>,
       },
       computed: {
         dots() {
-          return Math.ceil(this.mentors.length / 2);
+          return this.mentors.length;
         },
       },
       methods: {
         signIn() {
-          window.location.href = 'login.html';
+          window.location.href = 'login.php';
         },
-        signUp() {},
-        joinUs() {},
-        bookSession(id) {},
-        remindMe(id) {},
-        joinSession(id) {},
+        bookSession() {
+          window.location.href = 'learning.php';
+        },
+        remindMe() {
+          window.location.href = 'event.php';
+        },
+        joinSession() {
+          window.location.href = 'event.php';
+        },
         nextSlide() {
           if (this.currentSlide < this.dots - 1) {
             this.currentSlide++;
@@ -159,6 +133,7 @@
       },
     });
   </script>
+
 </body>
 
 </html>
