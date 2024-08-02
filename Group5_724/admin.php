8ac530cc996d7ef591c663b1fc5df5718396f2cf
Zimu <55948841+zimuli121@users.noreply.github.com>
2024-08-02 14:34:24 +0100

    update

--- Group5_724/admin.php
+++ Group5_724/admin.php
@@ -11,24 +11,14 @@
 <body>
 <header>
         <nav>
-            <div class="logo"><a href="post.php">StudentPreneur</a></div>
-            <a href="post.php">Home</a>
-            <a href="learning.php">Learning</a>
-            <a href="event.php">Events</a>
-            <a href="globalConnections.php">Global Connections</a>
-            <div class="icons">
-                <a href="teamprofile.php">🧑‍🧒</a>
-                <a href="message.php">💬</a>
-                <a href="profile.php">👤</a>
-                <a href="teamup1.php"><button>Team Up</button></a>
-            </div>
+            <div class="logo"><a href="homepage.php">StudentPreneur</a></div> 
         </nav>
     </header>
 
   <div class="main-content">
     <aside class="sidebar-content">
       <div class="sidebar-header">
-        <a href="home.php" class="heading">
+        <a href="admin.php" class="heading">
           <h1>Admin Dashboard</h1>
         </a>
       </div>
